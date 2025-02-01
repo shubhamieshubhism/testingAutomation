70a1df16f56ddb3e29a7327d275484c5ff4b56f1
shubhamieshubhism <shubhamtirpude20@gmail.com>
2025-02-01 14:15:44 +0530

    Longest SubString without repeating character

---
+++ src/test/java/leetCode/longestSubstringwithoutReapeatingChar.java
@@ -0,0 +1,24 @@
+public class longestSubstringwithoutReapeatingChar {
+    public static int LongestSubStringWithoutReapeatingChar(String str){
+        int n = str.length();
+        int left = 0;
+        int maxLength = 0;
+        LinkedHashSet<Character>charSet = new LinkedHashSet<>();
+        for(int right = 0 ; right < n ; right++){
+            if(!charSet.contains(str.charAt(right))){
+                charSet.add(str.charAt(right));
+                maxLength = Math.max(maxLength, right-left+1);
+            }else{
+                while(charSet.contains(str.charAt(right))){
+                    charSet.remove(str.charAt(left));
+                    left++;
+                }
+                charSet.add(str.charAt(right));
+            }
+        }
+        return maxLength;
+    }
+    
+    
+    
+}
