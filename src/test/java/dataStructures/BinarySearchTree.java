0227fef38e0010a6d0ed14c7dce67768518d9137
shubhamieshubhism <shubhamtirpude20@gmail.com>
2025-02-18 15:46:10 +0530

    fresh commit v2

---
+++ src/test/java/dataStructures/BinarySearchTree.java
@@ -0,0 +1,52 @@
+package dataStructures;
+
+public class BinarySearchTree {
+    public Node root;
+
+    class Node{
+        public int value;
+        public Node left;
+        public Node right;
+        Node(int value){
+            this.value=value;
+        }
+    }
+    public boolean insert(int value){
+       Node newNode = new Node(value);
+       if(root == null){
+           root = newNode;
+           return true;
+       }
+       Node temp = root;
+       while(true){
+           if(newNode.value==temp.value) return false;
+           if(newNode.value<temp.value){
+               if(temp.left==null){
+                   temp.left=newNode;
+                   return true;
+               }
+               temp=temp.left;
+           }else{
+               if(temp.right==null){
+                   temp.right=newNode;
+                   return true;
+               }
+               temp = temp.right;
+           }
+       }
+    }
+
+    public boolean contains(int value){
+        Node temp = root;
+        while(temp!=null){
+            if(value<temp.value){
+                temp=temp.left;
+            } else if (value>temp.value) {
+                temp=temp.right;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
