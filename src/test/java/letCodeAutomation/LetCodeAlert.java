4a458f57bf32d8aa5932f7efd0ad121a3338eab1
shubhamieshubhism <shubhamtirpude20@gmail.com>
2025-02-25 21:53:41 +0530

    Alert commit

--- src/test/java/letCodeAutomation/LetCodeAlert.java
+++ src/test/java/letCodeAutomation/LetCodeAlert.java
@@ -1,4 +1,37 @@
 package letCodeAutomation;
 
-public class LetCodeAlert {
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class LetCodeAlert extends BeforeAndAfterSetup{
+    @Test
+    public void alert() throws InterruptedException {
+        driver.findElement(By.xpath("//a[.=' Dialog ']")).click();
+
+        //accepting an alert
+        driver.findElement(By.xpath("//button[.='Simple Alert']")).click();
+        Alert simpleAlert = driver.switchTo().alert();
+        simpleAlert.accept();
+
+        //dismissing the alert
+        driver.findElement(By.xpath("//button[.='Confirm Alert']")).click();
+        Alert confirmAlert = driver.switchTo().alert();
+        System.out.println(confirmAlert.getText());
+        confirmAlert.dismiss();
+
+        //Type your name & accept
+        driver.findElement(By.xpath("//button[.='Prompt Alert']")).click();
+        Alert promtAlert = driver.switchTo().alert();
+        promtAlert.sendKeys("Shubham");
+        SoftAssert sa = new SoftAssert();
+        sa.assertTrue(true,"Your name is: shubham");
+        sa.assertAll();
+        Thread.sleep(5000);
+
+        System.out.println(promtAlert.getText());
+        promtAlert.accept();
+    }
+
 }
